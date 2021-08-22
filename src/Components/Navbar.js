db5b1c53636473a640945aa7d6405e865e77de6f
Shaun Valentine <valensh1@yahoo.com>
2021-08-21 23:57:05 -0700

    Navbar complete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,38 +3,41 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className='navbar'>
-      <ul className='navbar--ul'>
-        <li>
-          <Link to='/scores' className='navbar--ul-link'>
-            Scores
-          </Link>
-        </li>
-        <li>
-          <Link to='/news' className='navbar--ul-link'>
-            News
-          </Link>
-        </li>
-        <li>
-          <Link to='/video' className='navbar--ul-link'>
-            Video
-          </Link>
-        </li>
-        <li>
-          <Link to='/stats' className='navbar--ul-link'>
-            Stats
-          </Link>
-        </li>
-        <li>
-          <Link to='/fantasy' className='navbar--ul-link'>
-            Fantasy
-          </Link>
-        </li>
-        <li>
-          <Link to='/shop' className='navbar--ul-link'>
-            Shop
-          </Link>
-        </li>
-      </ul>
+      <img id='nhl-logo' src='./images/NHLLogo.png' alt='NHL Logo' />
+      <div className='ul-container'>
+        <ul className='navbar--ul'>
+          <li>
+            <Link to='/scores' className='navbar--ul-link'>
+              Scores
+            </Link>
+          </li>
+          <li>
+            <Link to='/news' className='navbar--ul-link'>
+              News
+            </Link>
+          </li>
+          <li>
+            <Link to='/video' className='navbar--ul-link'>
+              Video
+            </Link>
+          </li>
+          <li>
+            <Link to='/stats' className='navbar--ul-link'>
+              Stats
+            </Link>
+          </li>
+          <li>
+            <Link to='/fantasy' className='navbar--ul-link'>
+              Fantasy
+            </Link>
+          </li>
+          <li>
+            <Link to='/shop' className='navbar--ul-link'>
+              Shop
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
